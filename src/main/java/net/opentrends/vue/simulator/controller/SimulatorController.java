cd679b4fb04579e81ee4126fc82d232777c128b8
Roger Abad <rabad@opentreds.net>
2021-04-16 11:44:54 +0200

    api changes

--- src/main/java/net/opentrends/vue/simulator/controller/SimulatorController.java
+++ src/main/java/net/opentrends/vue/simulator/controller/SimulatorController.java
@@ -23,67 +23,64 @@ public class SimulatorController {
 
 	@Autowired
 	private SimulatorService simulatorService;
-	
+
 	@ApiOperation(value = "Configuration from VUE reader simulator ")
 	@GetMapping("/{serialNumber}/v2.4/config_reader")
-	public ResponseEntity<?> config(
-			@ApiParam(value = "VUE Reader Simulator SN")
-			@PathVariable String serialNumber) {
+	public ResponseEntity<ConfigReaderResponseTO> config(@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
 		ConfigReaderResponseTO configResponse = new ConfigReaderResponseTO();
 		configResponse.setConfigReaderTO(simulatorService.getConfigReader(serialNumber));
 		if (configResponse.getConfigReaderTO() == null) {
 			return ResponseEntity.notFound().build();
-		}else {
+		} else {
 			return ResponseEntity.ok(configResponse);
 		}
 	}
 
 	@ApiOperation(value = "Status from VUE reader simulator ")
 	@GetMapping("/{serialNumber}/status")
-	public ResponseEntity <?> readerStatus(
-			@ApiParam(value = "VUE Reader Simulator SN")
-			@PathVariable String serialNumber) {
+	public ResponseEntity<StatusResponseTO> readerStatus(
+			@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
 		StatusResponseTO statusResponse = new StatusResponseTO();
 		statusResponse.setStatusTO(simulatorService.getStatusConfig(serialNumber));
 		if (statusResponse.getStatusTO() == null) {
 			return ResponseEntity.notFound().build();
-		}else {
+		} else {
 			return ResponseEntity.ok(statusResponse);
 		}
 	}
-	
+
 	@ApiOperation(value = " ")
 	@GetMapping("/{serialNumber}/v2.4/reader_date_and_time")
 	public ResponseEntity<DateTimeResponseTO> readerDateAndTime(
-			@ApiParam(value = "VUE Reader Simulator SN")
-			@PathVariable String serialNumber) {
+			@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
 		return ResponseEntity.ok(new DateTimeResponseTO());
 	}
-	
+
 	@ApiOperation(value = "Test from VUE reader simulator ")
 	@PostMapping("/{serialNumber}/v2.4/cassette_processes")
 	public String cassetteProcesses1(@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
 		return "statusTO";
 	}
-	
+
 	@ApiOperation(value = "Test from VUE reader simulator ")
 	@GetMapping("/{serialNumber}/v2.4/cassette_processes")
-	public ResponseEntity<?> cassetteProcesses2(@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
-		CassetteProcessesResponseTO  cassetteProcessesResponse = new CassetteProcessesResponseTO();
+	public ResponseEntity<CassetteProcessesResponseTO> cassetteProcesses2(
+			@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
+		CassetteProcessesResponseTO cassetteProcessesResponse = new CassetteProcessesResponseTO();
 		cassetteProcessesResponse.setResultTO(simulatorService.getCassetteProcesses(serialNumber));
 		if (cassetteProcessesResponse.getResultTO() == null) {
 			return ResponseEntity.notFound().build();
-		}else {
+		} else {
 			return ResponseEntity.ok(cassetteProcessesResponse);
 		}
 	}
-	
+
 	@ApiOperation(value = "Image test from VUE reader simulator ")
 	@GetMapping("/{serialNumber}/v2.4/images")
 	public String images(@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
 		return "statusTO";
 	}
-	
+
 	@ApiOperation(value = "Cancel timed scan ")
 	@PutMapping("/{serialNumber}/v2.4/images")
 	public String cancelTimedScant(@ApiParam(value = "VUE Reader Simulator SN") @PathVariable String serialNumber) {
@@ -91,4 +88,3 @@ public class SimulatorController {
 	}
 
 }
-
